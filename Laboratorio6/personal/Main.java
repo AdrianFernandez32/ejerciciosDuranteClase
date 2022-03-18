f34ab88554be6918f58bf2ad6b9917f64654d63c
Adrián Fernández <oscar.adrian.fersa@gmail.com>
2022-03-18 14:57:44 -0700

    Objects

--- Laboratorio6/personal/Main.java
+++ Laboratorio6/personal/Main.java
@@ -1,8 +1,12 @@
 package Laboratorio6.personal;
 
+import Laboratorio6.cursos.Materia;
+
 public class Main {
     public static void main (String []args){
-        
+        Estudiante Manuel = new Estudiante("Manuelito");
+        Profesor Carlos = new Profesor("Alemán");
+        Matera Matematicas = new Mater
     }
     
 }
