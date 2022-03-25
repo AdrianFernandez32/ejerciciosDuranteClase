c2eed6729b0605259b6c3a3453547827ef740b0c
Adrián Fernández <oscar.adrian.fersa@gmail.com>
2022-03-25 08:38:39 -0700

    Agregué clase university

---
+++ Laboratorio6/core/university.java
@@ -0,0 +1,14 @@
+package Laboratorio6.core;
+
+import java.util.ArrayList;
+
+import Laboratorio6.personal.Estudiante;
+
+public class university {
+    String NombreUniversidad;
+    int ID;
+    ArrayList <Estudiante> EstudiantesRegistrados = new ArrayList<>();
+    String datos = "";//Ubicacion del archivo
+    String Settings = ""; //Ubicacion del archivo con 0
+    
+}
